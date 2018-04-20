746cc193878d90ce66b3a57b5051daac6ddaa68c
Eric Parker <theriex@gmail.com>
2018-04-20 10:21:50 -0400

    Slide name in from the left and adjust size

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -7,6 +7,8 @@ var app = {},
 app = (function () {
     "use strict";
 
+    var st = {};  //state variables
+
     //Sometimes there's a significant lag loading the fonts, and if
     //that is done in the index page then you are just sitting there
     //waiting for the site to display.  That's annoying and looks bad.
@@ -29,9 +31,48 @@ app = (function () {
     }
 
 
+    function objvals (obj) {
+        var str = "";
+        Object.keys(obj).forEach(function (key) {
+            if(str) {
+                str += ", "; }
+            str += key + ": " + obj[key]; });
+        return str;
+    }
+
+
+    function sizeNameToFit () {
+        var namediv = jt.byId("namediv"), wo, sv;
+        namediv.style.marginLeft = "0px";
+        st.snf = st.snf || {retries:[0, 1500, 2500, 7000], rti:0,
+                            sc:{min:340, max:1000},
+                            fs:{min:22, max:48, cur:24},
+                            ls:{min:0.1, max:0.4, cur:0.1}};
+        wo = st.snf.sc;
+        wo.cur = window.innerWidth;
+        wo.cur = Math.min(wo.cur, wo.max);
+        wo.cur = Math.max(wo.cur, wo.min);
+        //convert to scaled value in the range of 0 to 1
+        sv = (wo.cur - wo.min) / (wo.max - wo.min)
+        jt.log("sizeNameToFit sv: " + sv);
+        wo = st.snf.fs;
+        wo.cur = wo.min + (sv * (wo.max - wo.min));
+        jt.log("sizeNameToFit fs: " + objvals(wo));
+        namediv.style.fontSize = wo.cur + "px";
+        wo = st.snf.ls;
+        wo.cur = wo.min + (sv * (wo.max - wo.min));
+        jt.log("sizeNameToFit ls: " + objvals(wo));
+        namediv.style.letterSpacing = wo.cur + "em";
+        if(st.snf.rti < st.snf.retries.length) {
+            st.snf.rti += 1;
+            setTimeout(sizeNameToFit, st.snf.retries[st.snf.rti]); }
+    }
+
+
     function init () {
         jtminjsDecorateWithUtilities(jt);
         addFontSupport();
+        sizeNameToFit();
     }
 
 
