c1499a49e6ad47f3d24a332e14a3f56a551f7bcc
Eric Parker <theriex@gmail.com>
2019-09-13 16:26:51 -0400

    Site 2.0 initial revision

--- docroot/js/app.js
+++ docroot/js/app.js
@@ -1,8 +1,23 @@
 /*global jtminjsDecorateWithUtilities, document, setTimeout, window */
-/*jslint browser, multivar, white, fudge, for */
+/*jslint browser, white, fudge, for, long */
 
-var app = {},
-    jt = {};
+var app = {};
+var jt = {};
+var menu = [
+    {hash:"windisp", name:"Window Displays", type:"pics", content:[
+        {sec:"1_temp.jpg", pics:[
+            "2.jpg", "5.jpg", "8-798A9322.jpg", "17-798A9402_3_4_5_6_7_8.jpg", 
+            "12-798A9410_1_2.jpg"]},
+        {sec:"etc_windows-71.jpg", pics:[
+            "etc_windows-57.jpg", "etc_windows-17.jpg", "etc_windows-24.jpg", 
+            "etc_windows-2.jpg", "etc_windows-5.jpg"]}]},
+    {hash:"sculpture", name:"Sculpture", type:"pics"},
+    //{hash:"events", name:"Events", type:"pics"},
+    {hash:"production", name:"Production", type:"pics"},
+    {hash:"bio", name:"Bio", type:"pages", file:"bio.html"},
+    {hash:"cv", name:"CV", type:"pages", file:"cv.html"},
+    {hash:"email", name:"Contact", type:"email"}];
+
 
 app = (function () {
     "use strict";
@@ -17,12 +32,19 @@ app = (function () {
     //cause a blink in the display, but it's worth it to not have that
     //hideous lag occasionally.
     function addFontSupport () {
+        //namediv
         var fontlink = document.createElement("link");
-        fontlink.href = "//fonts.googleapis.com/css?family=Boogaloo";
+        fontlink.href = "//fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext";
         fontlink.rel = "stylesheet";
         //fontlink.type = "text/css";
         document.getElementsByTagName("head")[0].appendChild(fontlink);
         jt.log("added stylesheet " + fontlink.href);
+        //menulinksdiv...  Quicksand has rendering issues
+        fontlink = document.createElement("link");
+        fontlink.href = "https://fonts.googleapis.com/css?family=Quicksand&display=swap";
+        document.getElementsByTagName("head")[0].appendChild(fontlink);
+        jt.log("added stylesheet " + fontlink.href);
+        //body
         fontlink = document.createElement("link");
         fontlink.href = "//fonts.googleapis.com/css?family=Open+Sans:400,700";
         fontlink.rel = "stylesheet";
@@ -32,36 +54,170 @@ app = (function () {
     }
 
 
-    function objvals (obj) {
-        var str = "";
-        Object.keys(obj).forEach(function (key) {
-            if(str) {
-                str += ", "; }
-            str += key + ": " + obj[key]; });
-        return str;
+    //minorly obfuscate this source to avoid crawler spam
+    function getContactLink () {
+        var pc = ["m", "a", "i",
+                  "l", "t", "o",
+                  "@"];
+        var contact = pc.join("").slice(0, -1) + ":" + st.em + 
+            pc[pc.length - 1] + st.sechome + "?subject=" +
+            jt.dquotenc("website contact");
+        return contact;
     }
 
 
+    function showMenu () {
+        var html = "";
+        menu.forEach(function (mi, idx) {
+            var ho = {};
+            switch(mi.type) {
+            case "email":
+                ho.href = getContactLink();
+                break;
+            case "pics":
+                ho.href = "#" + mi.hash;
+                ho.onclick = jt.fs("app.showcontent(" + idx + ")");
+                break;
+            default: //assume "pages" file
+                ho.href = "pages/" + mi.file;
+                ho.onclick = jt.fs("app.showcontent(" + idx + ")"); }
+            html += jt.tac2html(
+                ["div",
+                 ["a", ho, mi.name]]); });
+        jt.out("menulinksdiv", html);
+        jt.out("contentdiv", jt.tac2html(
+            ["img", {cla:"dispimg", id:"sigpic", src:"img/FlyONSandwich2.jpeg",
+                     onclick:jt.fs("app.togimgexp('sigpic')")}]));
+    }
+
+
+    function relativeToAbsolute (url) {
+        var loc = window.location.href;
+        loc = loc.slice(0, loc.lastIndexOf("/") + 1);
+        return loc + url;
+    }
+
+
+    function displayDocContent (url, html) {
+        if(!html || !html.trim()) {
+            html = url + " contains no text."; }
+        var idx = html.indexOf("<body");
+        if(idx > 0) {
+            html = html.slice(idx);
+            idx = html.indexOf(">");
+            html = html.slice(idx + 1, html.indexOf("</body")); }
+        jt.out("contentdiv", html);
+    }
+
+
+    function showFileContents (cdef) {
+        if(!cdef.file) {
+            return jt.out("contentdiv", "No file specified for " + cdef.name); }
+        var url = relativeToAbsolute(cdef.type + "/" + cdef.file);
+        jt.out("contentdiv", "Retrieving " + url + " ...");
+        //requires a local web server (cd docroot, http-server, localhost:8080)
+        jt.request("GET", url, null,
+                   function (resp) {
+                       displayDocContent(url, resp); },
+                   function (code, errtxt) {
+                       displayDocContent(url, String(code) + ": " + errtxt); },
+                   jt.semaphore("app.showFileContents"));
+    }
+
+
+    function sectionPicLink (mi, si) {
+        var md = menu[mi];
+        var sd = md.content[si];
+        return jt.tac2html(
+            ["a", {href:"#expand_" + si,
+                   onclick:jt.fs("app.expsec(" + mi + "," + si + ")")},
+             [["img", {src:"img/" + md.hash + "/" + sd.sec, cla:"idximg"}],
+              ["span", {cla:"arrowspanright"}, "&gt;"]]]);
+    }
+
+
+    function showPics (cdef, mi) {
+        if(!cdef.content || !cdef.content.length) {
+            return jt.out("contentdiv", cdef.name + " being revised, " + 
+                   "please check back in a few days."); }
+        var html = "";
+        cdef.content.forEach(function (ignore, idx) {
+            html += jt.tac2html(
+                ["div", {cla:"secpicdiv", id:"secpicdiv" + idx},
+                 sectionPicLink(mi, idx)]); });
+        jt.out("contentdiv", html);
+    }
+
+
+    function expandPicSection (mi, si) {
+        var md = menu[mi];
+        var sd = md.content[si];
+        var html = jt.tac2html(
+            ["div", {cla:"sectionclosediv"},
+             ["a", {href:"#collapse_" + si,
+                    onclick:jt.fs("app.closec(" + mi + "," + si + ")")},
+              ["span", {cla:"arrowspanleft"}, "&lt;"]]]);
+        var imgidb = "dimg_m" + mi + "s" + si;
+        var srcb = "img/" + md.hash + "/";
+        html += jt.tac2html(
+            ["img", {cla:"dispimg", id:imgidb, src:srcb + sd.sec,
+                     onclick:jt.fs("app.togimgexp('" + imgidb + "')")}]);
+        sd.pics.forEach(function (pn, idx) {
+            var iid = imgidb + "c" + idx;
+            html += jt.tac2html(
+                ["img", {cla:"dispimg", id:iid, src:srcb + pn,
+                         onclick:jt.fs("app.togimgexp('" + iid + "')")}]); });
+        jt.out("secpicdiv" + si, html);
+    }
+
+
+    function collapsePicSection (mi, si) {
+        jt.out("secpicdiv" + si, sectionPicLink(mi, si));
+    }
+
+
+    function showContent (idx) {
+        var cdef = menu[idx];
+        jt.out("menulinksdiv", jt.tac2html(
+            ["div",
+             ["a", {href:"#menu", onclick:jt.fs("app.showmenu()")},
+              [["span", {cla:"arrowspanleft"}, "&lt;"],
+               cdef.name]]]));
+        if(cdef.type === "pics") {
+            showPics(cdef, idx); }
+        else {
+            showFileContents(cdef); }
+    }
+
+
+    // function objvals (obj) {
+    //     var str = "";
+    //     Object.keys(obj).forEach(function (key) {
+    //         if(str) {
+    //             str += ", "; }
+    //         str += key + ": " + obj[key]; });
+    //     return str;
+    // }
+
+
     function sizeNameToFit () {
-        var namediv = jt.byId("namediv"), wo, sv;
+        var namediv = jt.byId("namediv"); var wo; var sv;
         namediv.style.marginLeft = "0px";
         st.snf = st.snf || {retries:[0, 1500, 2500, 7000], rti:0,
                             sc:{min:340, max:1000},
-                            fs:{min:22, max:48, cur:24},
+                            fs:{min:20, max:48, cur:24},
                             ls:{min:0.1, max:0.4, cur:0.1}};
         wo = st.snf.sc;
         wo.cur = window.innerWidth;
         wo.cur = Math.min(wo.cur, wo.max);
         wo.cur = Math.max(wo.cur, wo.min);
         //convert to scaled value in the range of 0 to 1
-        sv = (wo.cur - wo.min) / (wo.max - wo.min)
+        sv = (wo.cur - wo.min) / (wo.max - wo.min);
         //jt.log("sizeNameToFit sv: " + sv);
         wo = st.snf.fs;
         wo.cur = wo.min + (sv * (wo.max - wo.min));
         //jt.log("sizeNameToFit fs: " + objvals(wo));
         namediv.style.fontSize = wo.cur + "px";
-        jt.byId("sitepicsdiv").style.paddingTop = (wo.cur + 50) + "px";
-        jt.byId("bestigdiv").style.paddingTop = (wo.cur + 50) + "px";
         wo = st.snf.ls;
         wo.cur = wo.min + (sv * (wo.max - wo.min));
         //jt.log("sizeNameToFit ls: " + objvals(wo));
@@ -72,80 +228,32 @@ app = (function () {
     }
 
 
-    function showEmailContact () {
-        var div = jt.byId("maildiv"),
-            pc = ["m", "a", "i",   //fuckin bots.  Worth a shot.
-                  "l", "t", "o",
-                  "@"];
-        div.style.marginRight = "0px";
-        div.style.transform = "rotate(0deg)";
-        setTimeout(function () {
-            pc = pc.join("").slice(0, -1) + ":" + st.em + pc[pc.length - 1];
-            pc += st.sechome;
-            jt.log("Activating mail link");
-            div.innerHTML = jt.tac2html(
-                ["a", {href:pc}, div.innerHTML]); }, 1500);
-    }
-
-
-    function externalLinkClick (event) {
-        var src;
-        jt.evtend(event);
-        src = event.target || event.srcElement;
-        if(src) {
-            if(!src.href) {  //event src gives you the img
-                src = src.parentNode; }
-            window.open(src.href); }
-    }
-
-
-    function showInstagramLink () {
-        var div = jt.byId("instdiv"),
-            link = div.children[0];
-        div.style.marginRight = "0px";
-        div.style.transform = "rotate(0deg)";
-        jt.on(link, "click", externalLinkClick);
-    }
-
-
-    function toggleDisplayDivs () {
-        var igdiv = jt.byId("bestigdiv"),
-            spdiv = jt.byId("sitepicsdiv");
-        if(!spdiv.style.display || spdiv.style.display === "block") {
-            spdiv.style.display = "none";
-            igdiv.style.display = "block"; }
-        else {
-            spdiv.style.display = "block";
-            igdiv.style.display = "none"; }
-    }
-
-
-    function showSiteFlyLink () {
-        var div = jt.byId("flydiv"),
-            img = div.children[0];
-        div.style.marginRight = "0px";
-        div.style.transform = "rotate(0deg)";
-        jt.on(img, "click", toggleDisplayDivs);
-    }
-
-
-    function showContactInfo () {
-        showEmailContact();
-        showInstagramLink();
-        showSiteFlyLink();
-    }
-
-
     function init () {
         jtminjsDecorateWithUtilities(jt);
         addFontSupport();
+        showMenu();
         sizeNameToFit();
-        setTimeout(showContactInfo, 1200);
+    }
+
+
+    function toggleImageExpansion (imgid) {
+        var img = jt.byId(imgid);
+        if(!img) {
+            return jt.log("toggleImageExpansion no img id " + imgid); }
+        if(img.className.indexOf("dispimg") >= 0) {
+            img.className = "fullimg"; }
+        else {
+            img.className = "dispimg"; }
     }
 
 
 return {
-    init: function () { init(); }
-}
+    init: function () { init(); },
+    togimgexp: function (imgid) { toggleImageExpansion(imgid); },
+    showcontent: function (idx) { showContent(idx); },
+    showmenu: function () { showMenu(); },
+    expsec: function (mi, si) { expandPicSection(mi, si); },
+    closec: function (mi, si) { collapsePicSection(mi, si); }
+};
 }());
 
